026b4dd87e529b0a5496fa47199304bbb6bf11f0
Taib Izzat Samawi <taibizzat165@gmail.com>
2024-03-10 13:01:22 +0700

    feat: add ros_react package

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import Image from 'next/image'
+import styles from './page.module.css'
 
 export default function Home() {
     return (
         <main className = {styles.main}>
-        <div  className = {styles.description}>
+            <div className = {styles.description}>
                 <p>
                     Get started by editing&nbsp;
                     <code className = {styles.code}>src/app/page.tsx</code>
@@ -15,7 +15,7 @@ export default function Home() {
                         target = "_blank"
                         rel    = "noopener noreferrer"
                     >
-                        By{" "}
+                        By{' '}
                         <Image
                             src       = "/vercel.svg"
                             alt       = "Vercel Logo"
@@ -49,7 +49,10 @@ export default function Home() {
                     <h2>
                         Docs <span>-&gt;</span>
                     </h2>
-                    <p>Find in-depth information about Next.js features and API.</p>
+                    <p>
+                        Find in-depth information about Next.js features and
+                        API.
+                    </p>
                 </a>
 
                 <a
@@ -61,7 +64,10 @@ export default function Home() {
                     <h2>
                         Learn <span>-&gt;</span>
                     </h2>
-                    <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
+                    <p>
+                        Learn about Next.js in an interactive course
+                        with&nbsp;quizzes!
+                    </p>
                 </a>
 
                 <a
@@ -86,10 +92,11 @@ export default function Home() {
                         Deploy <span>-&gt;</span>
                     </h2>
                     <p>
-                        Instantly deploy your Next.js site to a shareable URL with Vercel.
+                        Instantly deploy your Next.js site to a shareable URL
+                        with Vercel.
                     </p>
                 </a>
             </div>
         </main>
-    );
+    )
 }
